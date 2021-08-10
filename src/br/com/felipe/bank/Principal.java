7a2d5d8cfaa2b96c1e5ad3837c996332454eb26b
felipeam10 <felipeam10@hotmail.com>
2021-08-10 15:49:24 -0300

    commit v3.1 buscando arquivos ou qualquer coisa

--- src/br/com/felipe/bank/Principal.java
+++ src/br/com/felipe/bank/Principal.java
@@ -4,25 +4,15 @@ import br.com.felipe.bank.model.Funcionario;
 import br.com.felipe.bank.model.Pagamento;
 import br.com.felipe.bank.model.Tipo;
 import br.com.felipe.bank.registrador.RegistroDePagamento;
+import br.com.felipe.bank.visualizador.OpcoesPagamento;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Hello intelliJ Ideia");
-        Funcionario jose = new Funcionario("Jose", 1, LocalDate.of(1990, 2, 10));
-        Funcionario maria = new Funcionario("Maria", 2, LocalDate.of(1991, 3, 11));
-        System.out.println(jose);
-        System.out.println(maria);
-
-        Pagamento almoco = new Pagamento(Tipo.DEBITO, 20, "Almoço no feijuca");
-        Pagamento videogame = new Pagamento(Tipo.CREDITO, 1000, "PS4");
-
-        List<Pagamento> pagamentos = Arrays.asList(almoco, videogame);
-
-        RegistroDePagamento registrador = new RegistroDePagamento();
-        registrador.registra(pagamentos);
+    public static void main(String[] args) throws IOException {
+        new OpcoesPagamento().mensagemDeBoasVindas();
     }
 }
