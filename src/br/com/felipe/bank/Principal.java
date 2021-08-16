1a272f74b9f5eeaaf21b21d3d04187a232aa5e63
felipeam10 <felipeam10@hotmail.com>
2021-08-16 11:24:28 -0300

    commit v6.1 utilizando templates

--- src/br/com/felipe/bank/Principal.java
+++ src/br/com/felipe/bank/Principal.java
@@ -1,5 +1,7 @@
 package br.com.felipe.bank;
 
+import br.com.felipe.bank.io.LeitorCSV;
+import br.com.felipe.bank.io.LeitorXML;
 import br.com.felipe.bank.model.Funcionario;
 import br.com.felipe.bank.model.Pagamento;
 import br.com.felipe.bank.model.Tipo;
@@ -13,6 +15,24 @@ import java.util.List;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-        new OpcoesPagamento().mensagemDeBoasVindas();
+//      new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitorCSV = new LeitorCSV();
+        List<Pagamento> pagamentos = leitorCSV.ler(1);
+
+        for (Pagamento pagamento:
+             pagamentos) {
+
+            double valorBruto = pagamento.getValor();
+
+            if (valorBruto > 100) {
+                double valorComAcrescimo = valorBruto * 1.1;
+                pagamento.setValor(valorComAcrescimo);
+            }
+
+            System.out.println(pagamento);
+
+        }
+
     }
 }
